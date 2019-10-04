449d14cf93f43dfabeeccd01bbe6341c3f4b347e
kent-wong <w02682@gmail.com>
2019-10-04 19:01:42 +0800

    Add file

---
+++ src/context/github/githubReducer.js
@@ -0,0 +1,21 @@
+import {
+  SEARCH_USERS,
+  GET_USER,
+  CLEAR_USERS,
+  GET_REPOS,
+  SET_LOADING,
+  SET_ALERT,
+  REMOVE_ALERT
+} from '../types';
+
+export default (state, action) => {
+  switch (action.type) {
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true
+      }
+    default:
+      return state;
+  }
+}
